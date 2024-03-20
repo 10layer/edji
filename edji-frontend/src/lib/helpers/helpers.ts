384a77e34fbf4e209ab2935c09164ff4a8643121
Jason Norwood-Young <jason@10layer.com>
2024-03-20 11:44:09 +0100

    Don't freak out!

--- edji-frontend/src/lib/helpers/helpers.ts
+++ edji-frontend/src/lib/helpers/helpers.ts
@@ -1,11 +1,15 @@
 Object.defineProperty(String.prototype, 'ucfirst', {
     value: function() {
+        if (this.length === 0) return this;
+        if (typeof this !== 'string') return this;
         return this.charAt(0).toUpperCase() + this.slice(1);
     }
 });
 
 Object.defineProperty(String.prototype, 'capitalize', {
     value: function() {
+        if (this.length === 0) return this;
+        if (typeof this !== 'string') return this;
         return this.split(' ').map(function(item: string) {
             return item.charAt(0).toUpperCase() + item.slice(1);
         }).join(' ');
